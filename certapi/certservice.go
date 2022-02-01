01ed4d2e650f46b8f74e43220374aecabee2ef67
Kalle M. Aagaard <git@k-moeller.dk>
2022-02-01 01:04:40 +0100

    serial

--- certapi/certservice.go
+++ certapi/certservice.go
@@ -13,6 +13,7 @@ type Item struct {
 }
 
 type CertService interface {
+	GetSerial(domain string, apikey APIKey) (serial string, err error)
 	GetItem(domain string, itemtype string, apikey APIKey) (item Item, err error)
 	GetItemInfo(domain string, itemtype string, apikey APIKey) (info ItemInfo, err error)
 }
